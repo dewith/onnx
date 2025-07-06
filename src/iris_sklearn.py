9c5af702b4e0c9010e18afd4a9def9f9a629150b
dewtih <dewithmiramon@gmail.com>
2025-07-06 17:40:42 -0500

    Little logging update

--- src/iris_sklearn.py
+++ src/iris_sklearn.py
@@ -16,7 +16,7 @@ from sklearn.tree import DecisionTreeClassifier
 def main():
     logging.basicConfig(
         level=logging.INFO,
-        format="%(asctime)s %(levelname)s: %(message)s",
+        format="%(asctime)s %(levelname)s | %(message)s",
         datefmt="%Y-%m-%d %H:%M:%S",
     )
     logger = logging.getLogger(__name__)
